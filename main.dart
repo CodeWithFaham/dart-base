96fd6bc902b0ac1aeac5e19fc22e66d07f117c9f
Muhammad Faham <fahambbs@gmail.com>
2025-09-18 12:23:09 -0700

    Add files via upload

--- main.dart
+++ main.dart
@@ -1,56 +1,81 @@
-//class
+import 'dart:io';
 
-class BackAccount{
-// variables
-var balance=0;
-var accountholder;
-var withdrawl;
+void main() {
+  //even or odd
 
-//Constructor 
-//special function
-//call it self object is created//
+  // stdout.write("Enter a number: ");
+  // int? number = int.parse(stdin.readLineSync()!);
 
-BackAccount(this.accountholder){
-  
-}
-void print_balance(){
-    print("parent class balance ${balance}");
+  // if (number % 2 == 0) {
+  //   print("$number is an even number.");
+  // } else {
+  //   print("$number is an odd number.");
+  // }
+
+  // student information
+
+  // String Name = "Zain";
+  // int Age = 20;
+  // String Grade = "A";
+  // String Address = "123 Main St, Cityville";
+
+  // print("Student Information:");
+  // print("Name: $Name");
+  // print("Age: $Age");
+  // print("Grade: $Grade");
+  // print("Address: $Address");
+
+  // simple calculator
+
+  // stdout.write("Enter first number: ");
+  // int? num1 = int.parse(stdin.readLineSync()!);
+  // stdout.write("Enter second number: ");
+  // int? num2 = int.parse(stdin.readLineSync()!);
+  // stdout.write("Enter an operator (+, -, *, /): ");
+  // String? operator = stdin.readLineSync();
+  // double result;
+  // switch (operator) {
+  //   case '+':
+  //     int result = num1 + num2;
+  //     print("Result: $result");
+  //     break;
+  //   case '-':
+  //     int result = num1 - num2;
+  //     print("Result: $result");
+  //     break;
+  //   case '*':
+  //     int result = num1 * num2;
+  //     print("Result: $result");
+  //     break;
+  //   case '/':
+  //     if (num2 != 0) {
+  //       result = num1 / num2;
+  //       print("Result: $result");
+  //     } else {
+  //       print("Error: Division by zero is not allowed.");
+  //     }
+  //     break;
+  //   default:
+  //     print("Error: Invalid operator.");
+  // }
+
+  // 2 ka table
+
+  // int number = 2; // The number for which you want the table
+
+  // print('Multiplication Table of $number:');
+
+  // for (int i = 1; i <= 10; i++) {
+  //   int result = number * i;
+  //   print('$number x $i = $result');
+  // }
+
+  // factorial of a number
+  stdout.write("Enter a number: ");
+  int? number = int.parse(stdin.readLineSync()!);
+  int factorial = 1;
+  for (int i = 1; i <= number; i++) {
+    factorial *= i;
   }
-
-
-
-
-// functions
-void deposit(int Amount){
-if(Amount>0){
-  balance+=Amount;
-}
-print("$accountholder Account balance is : $balance");
-}
-}
-class Digital_Account extends BackAccount{
-  Digital_Account(super.accountholder);
-  void print_balance(){
-    print("child class balance ${balance}");
-  }
-  
-}
-// functions
-void withdrawl(int Amount){
-
-var accountholder;
-print("$accountholder Account balance after is : $Amount");
-}
-
-
-
-void main(){
-var obj=BackAccount("Muhammad Faham");
-obj.deposit(1000);
-BackAccount obj1 = Digital_Account("zain kamran");
-withdrawl(1500);
-
-obj.print_balance();
-obj1.print_balance();
-
+  print("Factorial of $number is $factorial");
 }
